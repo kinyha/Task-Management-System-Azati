8e159f9bf0c822ffb6a1bf489d8465ca857e6b79
Bratchykau Uladzislau <kinyhaha@gmail.com>
2024-11-27 12:17:40 +0300

    Initial commit: Project setup

---
+++ src/main/kotlin/by/bratchykau/task_management_system/TaskManagementSystemApplication.kt
@@ -0,0 +1,11 @@
+package by.bratchykau.task_management_system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaskManagementSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<TaskManagementSystemApplication>(*args)
+}
---
+++ src/test/kotlin/by/bratchykau/task_management_system/TaskManagementSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package by.bratchykau.task_management_system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
